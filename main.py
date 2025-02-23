1977ca1e14285e6abbf20402684c9ebf17e2d43e
petrtimalev <petr.timalev@gmail.com>
2025-02-23 15:42:34 +1000

    update 23.02

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, HTTPException, Path, Query, Body
+from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
 from typing import Optional, List, Dict, Annotated
 from sqlalchemy.orm import Session
 
 from models import Base, User, Post
 from database import engine, session_local
-from schemas import UserCreate, PostCreate, PostResponse
+from schemas import UserCreate, User as DbUser, PostCreate, PostResponse
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
-def get_db():
+def get_db():  # подключение к базе
     db = session_local()
     try:
         yield db
@@ -19,61 +19,13 @@ def get_db():
         db.close()
 
 
+@app.post("/user/", response_model=DbUser)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
+    db_user = User(name=user.name, age=user.age)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
 
-@app.get("/items")
-async def items() -> List[Post]:
-    return [Post(**post) for post in posts]
-
-
-@app.post("/items/add")
-async def add_item(post: PostCreate) -> Post:
-    author = next((user for user in users if user['id'] == post.author_id), None)
-    if not author:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    new_post_id = len(posts) + 1
-
-    new_post = {'id': new_post_id, 'title': post.title, 'body': post.body, 'author': author}
-    posts.append(new_post)
-
-    return Post(**new_post)
-
-
-@app.post("/user/add") # добавление нового user
-async def user_add(user: Annotated[
-    UserCreate,
-    Body(..., example={
-        'name': 'UserName',
-        'age': 10
-    })
-
-]) -> User: # используем Annotated[Body]
-    new_user_id = len(users) + 1
-
-    new_user = {'id': new_user_id, 'name': user.name, 'age': user.age}
-    users.append(new_user)
-
-    return User(**new_user)
-
-@app.get("/items/{id}")  # используем Annotated[..., Path] в динамике{}
-async def items(id: Annotated[int, Path(..., title='Здесь указывается id поста', ge=1)]) -> Post:
-    for post in posts:
-        if post['id'] == id:
-            return Post(**post)
-    raise HTTPException(status_code=404, detail="Input number: not more 3!!!")
-
-
-@app.get("/search")  # используем Annotated[..., Query] после апперсандов(?, &)
-async def search(post_id: Annotated[
-    Optional[int],
-    Query(title="ID of post to search for", ge=1, le=50)
-]) -> Dict[str, Optional[Post]]:
-    if post_id:
-        for post in posts:
-            if post['id'] == post_id:
-                return {"data": Post(**post)}
-        raise HTTPException(status_code=404, detail="Input number: not more 3!!!")
-    else:
-        return {'data': None}
+    return db_user
 
 
--- models.py
+++ models.py
@@ -10,12 +10,14 @@ class User(Base):
     name = Column(String, index=True)
     age = Column(Integer)
 
-    class Post(Base):
-        __tablename__ = 'posts'
+class Post(Base):
+    __tablename__ = 'posts'
 
-        id = Column(Integer, primary_key=True, index=True)
-        title = Column(String, index=True)
-        body = Column(String)
-        author_id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    body = Column(String)
+    author_id = Column(Integer, ForeignKey('users.id'))
 
         author = relationship('User')
+    class Config:
+        orm_mode = True
